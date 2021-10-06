f67968e3f2cf69eced0abbf21774d8f2601b27ab
ROGELJ18 <89958488+ROGELJ18@users.noreply.github.com>
2021-10-06 14:40:58 -0500

    TRABAJO EN EQUIPO

---
+++ TRABAJO EN EQUIPO/main.js
@@ -0,0 +1,35 @@
+<script>
+ let num = parseInt(prompt("ingresa un numero"));
+
+function imprimir(num) 
+
+{
+    
+   var texto = '';
+   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+   var charactersLength = characters.length;
+   for ( var i = 0; i = num; i++ ) {
+      texto += characters.charAt(Math.floor(Math.random() * charactersLength));
+      
+   }
+   
+   return texto;
+}
+
+for ( var i = 0; i = num; i++ ) 
+{
+      console.log(imprimir(num))}
+      
+   {
+
+   }
+
+
+
+
+
+
+
+
+
+</script>
