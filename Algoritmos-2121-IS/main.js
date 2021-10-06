774796123da8d255910d75ee057c4149c8374afe
ROGELJ18 <89958488+ROGELJ18@users.noreply.github.com>
2021-10-06 14:33:37 -0500

    subir archivos final

---
+++ Algoritmos-2121-IS/main.js
@@ -0,0 +1,13 @@
+var tarea = "tortillas";
+
+console.log("paso 1: salir de mi casa");
+
+if (tortillas == "si hay"){
+console.log("paso 2: comprarlas ahi");
+}else{
+    console.log("paso 2: buscar en otro lugar");
+}
+
+console.log("paso 3: comprarlas");
+console.log("paso 4: pagar");
+console.log("regresar a mi casa");
