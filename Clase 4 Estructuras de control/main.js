9e77d6bd3c6af481c3aee1a7bde49fb3cd2245d8
ROGELJ18 <89958488+ROGELJ18@users.noreply.github.com>
2021-10-06 14:35:07 -0500

    clase 4

---
+++ Clase 4 Estructuras de control/main.js
@@ -0,0 +1,12 @@
+var n1 = 10;
+var n2 = 5;
+var operacion1 = "resta";
+var operacion2 = "division";
+var total1 = 0;
+var total2 = 0;
+
+if(operacion1 == "suma"){
+    total1 = n1 + n2;
+}else{
+    total1 = n1 - n2;
+}
